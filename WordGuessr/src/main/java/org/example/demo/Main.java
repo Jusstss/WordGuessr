9c2ec20a175d9b1bc2ae3f8637f6a966ed1a41de
jusm0245 <jusm0245@uosis.mif.vu.lt>
2024-05-29 16:56:56 +0300

    screen size

--- WordGuessr/src/main/java/org/example/demo/Main.java
+++ WordGuessr/src/main/java/org/example/demo/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
 
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("game-screen.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400); 
+        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
         stage.setTitle("WordGuessr");
         stage.setScene(scene);
         stage.show();
